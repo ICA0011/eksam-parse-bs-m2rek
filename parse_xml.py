d00a962610f663e03041ae64af92cdc249845caa
Marek <marekott@hot.ee>
2022-05-26 12:40:21 +0300

    1

--- parse_xml.py
+++ parse_xml.py
@@ -1,12 +1,14 @@
+from dataclasses import dataclass
 from bs4 import BeautifulSoup
 import requests
-import lxml
+import xml
+
 
 def parse_xml():
   url = "http://upload.itcollege.ee/~aleksei/test.xml"
   xml_content = requests.get(url).content
   soup = BeautifulSoup(xml_content,'xml')
   
-  # your code here
-  
+  result = soup.find_all('data')
+
   return result
