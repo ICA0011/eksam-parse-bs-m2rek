5da44548188b2d7cbe6e0f528b9ab8421fdec956
Marek <marekott@hot.ee>
2022-05-26 12:48:17 +0300

    3

--- parse_xml.py
+++ parse_xml.py
@@ -4,12 +4,12 @@ import requests
 import xml
 
 
-def parse_xml(url):
+def parse_xml():
   url = "http://upload.itcollege.ee/~aleksei/test.xml"
   xml_content = requests.get(url).content
   print(xml_content)
   soup = BeautifulSoup(xml_content,'xml')
   
-  result = soup.find_all('data')
+  result = soup.find_all(soup)
 
   return result
